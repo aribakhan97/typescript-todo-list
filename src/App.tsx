c78e646353f0426381694d1b7749ba35971a73a5
aribakhan97 <akhan41@binghamton.edu>
2021-01-28 14:32:57 -0500

    added css

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import { TodoListItem } from "./TodoListItem";
 
 
-type Todo = {
-  text: string,
-  complete: boolean
-}
 
 
-const todos = [
+
+const todos: Array<Todo> = [
   { text: "Walk the dog", complete: true }, 
   { text: "Write app", complete: false } 
 ];
 
 
 const App: React.FC = () => {
-  return <TodoListItem />;
+  return (
+  <React.Fragment>
+
+    <TodoListItem todo={todos[0]} />
+    <TodoListItem todo={todos[1]} />
+
+  </React.Fragment>;
+  );
 };
 
 export default App;
