54b5160843473a389a80105c1074471b2aa3359b
aribakhan97 <akhan41@binghamton.edu>
2021-01-28 14:17:00 -0500

    starting creating todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TodoListItem } from "./TodoListItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+type Todo = {
+  text: string,
+  complete: boolean
 }
+
+
+const todos = [
+  { text: "Walk the dog", complete: true }, 
+  { text: "Write app", complete: false } 
+];
+
+
+const App: React.FC = () => {
+  return <TodoListItem />;
+};
 
 export default App;
